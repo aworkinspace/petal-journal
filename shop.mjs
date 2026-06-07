e6880c498af8293f2c2ec8add4dcd421e9e556b8
A <anushri21@gmail.com>
2026-06-06 22:00:45 -0400

    Fix typo in Sasuke sound effect ID

--- shop.mjs
+++ shop.mjs
@@ -17,7 +17,7 @@ const shopItems = [
   { id: "sfx_usuratonkachi", name: "SFX: Usuratonkachi", type: "sfx", price: 50, icon: "assets/sasuke_nendo.png" },
   { id: "sfx_notazenin", name: "SFX: Not A Zenin", type: "sfx", price: 50, icon: "assets/toji_nendo.png" },
   { id: "sfx_sukunalaugh", name: "SFX: Sukuna's Laugh", type: "sfx", price: 50, icon: "assets/sukuna_nendo.png" },
-  { id: "sfx_sasukeusayingnaruto", name: "SFX: NARUTOOOOO!", type: "sfx", price: 50, icon: "assets/sasuke_nendo.png" },
+  { id: "sfx_sasukesayingnaruto", name: "SFX: NARUTOOOOO!", type: "sfx", price: 50, icon: "assets/sasuke_nendo.png" },
   { id: "sfx_narutosayingsasuke", name: "SFX: SASUKEEEE!", type: "sfx", price: 50, icon: "assets/naruto_nendo.png" },
 ];
 
